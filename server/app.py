37b48e5ed914a4b540454abf3ff49bdeff1ac276
Malla Shyam kumar <mshyamkumar1911@gmail.com>
2026-04-08 20:29:25 +0530

    Create app.py

---
+++ server/app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from env import TrafficEnv
+
+# Initialize FastAPI app
+app = FastAPI()
+
+# Initialize your environment
+env = TrafficEnv()
+
+
+# -----------------------------
+# Input Model (VERY IMPORTANT)
+# -----------------------------
+class ActionInput(BaseModel):
+    action: str
+
+
+# -----------------------------
+# RESET ENDPOINT
+# -----------------------------
+@app.post("/reset")
+def reset():
+    state = env.reset()
+
+    return {
+        "observation": state,
+        "done": False
+    }
+
+
+# -----------------------------
+# STEP ENDPOINT
+# -----------------------------
+@app.post("/step")
+def step(action: ActionInput):
+    state, reward, done = env.step(action.action)
+
+    return {
+        "observation": state,
+        "reward": float(reward),
+        "done": done
+    }
